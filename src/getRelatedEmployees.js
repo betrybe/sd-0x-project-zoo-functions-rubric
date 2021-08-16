abd83aa83dab39013ddc7c88e75c08f4bc0b4491
Matheus Alexandre <alexandremhm@gmail.com>
2021-08-16 09:48:23 -0300

    Adiciona gabarito getRelatedEMployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees
+    .some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const verifyManager = isManager(managerId);
+  if (verifyManager){
+     return data.employees
+      .filter(({ managers }) => managers.includes(managerId))
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`)
+    }
+  throw new Error ('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
