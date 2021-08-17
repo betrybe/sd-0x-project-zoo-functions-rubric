42e355d3db237e4c9203c494335173ce81c74ca6
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-08-17 10:40:52 -0300

    Adiciona gabarito do calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,19 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+function countEntrants(clients) {
+  const child = clients.filter(({ age }) => age < 18).length;
+  const adult = clients.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = clients.filter(({ age }) => age >= 50).length;
+  return { child, adult, senior };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(clients = []) {
+  if (!Array.isArray(clients)) return 0;
+  const summary = countEntrants(clients);
+  return Object.entries(summary).reduce((acc, [type, ammount]) => {
+    const partial = prices[type] * ammount;
+    return partial + acc;
+  }, 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
