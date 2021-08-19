1314ad2139909c0ed50eb6d618e7891aa9a52a52
Matheus Alexandre <alexandremhm@gmail.com>
2021-08-19 08:31:37 -0300

    Update src/getRelatedEmployees.js
    Co-authored-by: Cleyton de Oliveira <cleytonoliveirapro@gmail.com>

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -7,12 +7,12 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   const verifyManager = isManager(managerId);
-  if (verifyManager){
-     return data.employees
+  if (verifyManager) {
+    return data.employees
       .filter(({ managers }) => managers.includes(managerId))
-      .map(({ firstName, lastName }) => `${firstName} ${lastName}`)
-    }
-  throw new Error ('O id inserido não é de uma pessoa colaboradora gerente!');
+      .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
