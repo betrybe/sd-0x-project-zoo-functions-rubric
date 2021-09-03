48c3f1af2afd8b5abd86fb9641472296c2c18a01
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-09-03 16:59:03 -0300

    Adiciona gabarito countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-
+  if (!animal) {
+    return data.species
+      .reduce((acc, { name, residents }) => ({ ...acc, [name]: residents.length }), {});
+  }
+  if (animal.genre) {
+    return data.species
+      .find((eachAnimal) => eachAnimal.name === animal.specie).residents
+      .filter(({ sex }) => sex === animal.genre).length;
+  }
+  return data.species.find((eachAnimal) => eachAnimal.name === animal.specie).residents.length;
 }
 
 module.exports = countAnimals;
